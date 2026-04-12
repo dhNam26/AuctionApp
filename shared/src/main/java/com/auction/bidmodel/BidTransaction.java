33a85f6a0cab91b05a59816e7d51153567858879
Nhan <nquangnhan216@gmail.com>
2026-04-12 17:18:57 +0700

    bid + bid transaction

---
+++ shared/src/main/java/com/auction/bidmodel/BidTransaction.java
@@ -0,0 +1,79 @@
+package com.auction.bidmodel;
+import java.time.LocalDateTime;
+
+public class BidTransaction {
+    private String id;
+    private String bidId;
+    private String auctionId;
+    private String bidderId;
+    private double bidAmount;
+    private LocalDateTime bidTime;
+    private boolean automaticBid;
+
+    public BidTransaction(String id, String bidId, String auctionId, String bidderId, double bidAmount, LocalDateTime bidTime, boolean automaticBid) {
+        this.id = id;
+        this.bidId = bidId;
+        this.auctionId = auctionId;
+        this.bidderId = bidderId;
+        this.bidAmount = bidAmount;
+        this.bidTime = bidTime;
+        this.automaticBid = automaticBid;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBidId() {
+        return bidId;
+    }
+
+    public void setBidId(String bidId) {
+        this.bidId = bidId;
+    }
+
+    public String getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(String auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public String getBidderId() {
+        return bidderId;
+    }
+
+    public void setBidderId(String bidderId) {
+        this.bidderId = bidderId;
+    }
+
+    public double getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(double bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public LocalDateTime getBidTime() {
+        return bidTime;
+    }
+
+    public void setBidTime(LocalDateTime bidTime) {
+        this.bidTime = bidTime;
+    }
+
+    public boolean isAutomaticBid() {
+        return automaticBid;
+    }
+
+    public void setAutomaticBid(boolean automaticBid) {
+        this.automaticBid = automaticBid;
+    }
+}
+
